34ddc46e3e84243658db9903494fc53a0d878983
advy99 <advy99@gmail.com>
2020-10-20 12:56:05 +0200

    prueba del proyecto

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/MainActivity.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/MainActivity.kt
@@ -2,10 +2,37 @@ package com.npi_grupo4.guiaestudiantes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var texto_casilla: EditText;
+    private lateinit var boton: Button;
+    private lateinit var salida: TextView;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto_casilla = findViewById(R.id.texto_prueba)
+        boton = findViewById(R.id.button)
+        salida = findViewById(R.id.textView)
+
+        boton.setOnClickListener { view->
+
+            when (view.id) {
+                R.id.button->mostrar()
+            }
+        }
+
     }
+
+    public fun mostrar(){
+        salida.setText(texto_casilla.getText())
+    }
+
+
 }
