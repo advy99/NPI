d7d106045b4a1bcd9238a4aa97ff1a960794e0ad
advy99 <advy99@gmail.com>
2020-10-20 14:03:13 +0200

    add activity para cada seccion de la App

---
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Grados.kt
@@ -0,0 +1,11 @@
+package com.npi_grupo4.guiaestudiantes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Grados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grados)
+    }
+}
