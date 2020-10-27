5b53db7354591ee58c1f8432df7346e3b7521455
advy99 <advy99@gmail.com>
2020-10-27 16:54:14 +0100

    tenemos movilidad con botones

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/InicioSesion.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/InicioSesion.kt
@@ -12,11 +12,6 @@ import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_inicio_sesion.*
 import kotlinx.android.synthetic.main.fragment_inicio_sesion.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [InicioSesion.newInstance] factory method to
