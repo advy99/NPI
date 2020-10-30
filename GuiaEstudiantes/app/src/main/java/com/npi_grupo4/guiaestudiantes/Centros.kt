30e84b833a80b879009f180442079250c25fc451
Alexmnzlms <alexmanza2@gmail.com>
2020-10-30 11:12:01 +0100

    Cambiado fragment mapa por webview

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Centros.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Centros.kt
@@ -1,85 +1,77 @@
 package com.npi_grupo4.guiaestudiantes
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import androidx.fragment.app.Fragment
-
+import android.net.http.SslError
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.*
+import android.webkit.SslErrorHandler
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import java.util.jar.Manifest
+
+// TODO: Rename parameter arguments, choose names that match
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 class Centros : Fragment() {
+    // TODO: Rename and change types of parameters
 
-    private var mLocationPermissionGranted = false
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val ETSIIT = LatLng(37.197055556, -3.624111111)
-        googleMap.addMarker(MarkerOptions().position(ETSIIT).title("Marker in ETSIIT"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ETSIIT))
+        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_centros, container, false)
-    }
+        val view: View = inflater.inflate(R.layout.fragment_centros, container, false)
+        val webview = view.findViewById(R.id.mapa_centros) as WebView
+        webview.getSettings().setJavaScriptEnabled(true)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        getLocationPermission();
-        mapFragment?.getMapAsync(callback)
-    }
+        webview.settings.javaScriptEnabled = true
+        webview.clearCache(true)
+        webview.webViewClient = object : WebViewClient() {
 
-    private fun getLocationPermission() {
-        val permissions = arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
-
-        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                mLocationPermissionGranted = true
-            } else {
-                ActivityCompat.requestPermissions(requireActivity(),permissions,LOCATION_PERMISSION_REQUEST_CODE)
+            override fun onPageFinished(view: WebView, url: String?) {
+                if (view.title == "") view.reload()
             }
-        } else {
-            ActivityCompat.requestPermissions(requireActivity(),permissions,LOCATION_PERMISSION_REQUEST_CODE)
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                webview.loadUrl("https://www.google.com/maps/d/embed?mid=1k7hR-nnrQivEs6-5LWRwE9aLRDMvJjaT")
+                return true
+            }
+
+            override fun onReceivedSslError(
+                    view: WebView,
+                    handler: SslErrorHandler,
+                    error: SslError
+            ) {
+                println("before handler")
+                handler.proceed()
+                println("after handler")
+            }
         }
+
+        webview.loadUrl("https://www.google.com/maps/d/embed?mid=1k7hR-nnrQivEs6-5LWRwE9aLRDMvJjaT")
+
+        return view
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        mLocationPermissionGranted = false
-
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE){
-            if(grantResults.size > 0){
-                for (i in 0 until grantResults.size){
-                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
-                        mLocationPermissionGranted = false
-                        return
+    companion object {
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                Centros().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
                     }
                 }
-                mLocationPermissionGranted = true
-            }
-        }
     }
 }
