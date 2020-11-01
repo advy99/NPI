00b09f3dc0001fcb1cbdbe3182f708aa4ad1cae5
advy99 <advy99@gmail.com>
2020-11-01 17:51:20 +0100

    estructura basica de gestos fisicos

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/MainActivity.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.npi_grupo4.guiaestudiantes
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.view.GestureDetector
@@ -15,16 +20,25 @@ import kotlin.math.abs
 private const val DEBUG_TAG = "Gestures"
 
 enum class Accion{
-    ATRAS, NINGUNA
+    ATRAS, NINGUNA, COMEDORES, CENTROS
 }
 
-class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, SensorEventListener {
+
     private lateinit var mDetector: GestureDetectorCompat;
+    private lateinit var sensorManager: SensorManager
 
     private var previousX: Float = 0f
     private var previousY: Float = 0f
 
     private var accion: Accion = Accion.NINGUNA
+
+    private lateinit var acelerometro: Sensor
+    private lateinit var gravedad: Sensor
+    private lateinit var giroscopio: Sensor
+    private lateinit var aceleracionLineal: Sensor
+    private lateinit var rotacion: Sensor
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +47,34 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, Ges
 
         mDetector = GestureDetectorCompat(this,this)
         mDetector.setOnDoubleTapListener(this)
+
+
+        this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+            acelerometro = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)?.let {
+            gravedad = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.let {
+            giroscopio = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
+            aceleracionLineal = it
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)?.let {
+            rotacion = it
+        }
+
     }
+
+
+    // PANTALLA
 
     override fun onDoubleTap(e: MotionEvent?): Boolean {
         Toast.makeText(this, "Doble toque",Toast.LENGTH_SHORT).show()
@@ -43,6 +84,9 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, Ges
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val x: Float = event.x
         val y: Float = event.y
+
+        val navigation = findNavController(this, R.id.nav_frag)
+
         if ( event.action == MotionEvent.ACTION_MOVE) {
             if ( abs(y - previousY) < 50 && previousX < x ) {
                 accion = Accion.ATRAS
@@ -51,7 +95,7 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, Ges
             }
         } else if ( event.action == MotionEvent.ACTION_UP) {
             if (accion == Accion.ATRAS){
-                val navigation = findNavController(this, R.id.nav_frag)
+
                 navigation.navigateUp()
             }
         }
@@ -109,6 +153,46 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, Ges
     override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
         Log.d(DEBUG_TAG, "onSingleTapConfirmed: $event")
         return true
+    }
+
+
+
+
+
+    // SENSORES HARDWARE
+
+    override fun onSensorChanged(event: SensorEvent){
+
+        val navigation = findNavController(this, R.id.nav_frag)
+
+        when(event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                Toast.makeText(this, " " + event.values[0] + " " + event.values[1] + " " + event.values[2],Toast.LENGTH_SHORT).show()
+            }
+
+            Sensor.TYPE_GRAVITY -> {
+
+            }
+
+            Sensor.TYPE_GYROSCOPE -> {
+
+            }
+
+            Sensor.TYPE_LINEAR_ACCELERATION -> {
+
+            }
+
+            Sensor.TYPE_ROTATION_VECTOR -> {
+
+            }
+
+        }
+
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int){
+
     }
 
 
