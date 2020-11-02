bf4b899745cb0e97d999a6a2fcba3670d84f2743
Alexmnzlms <alexmanza2@gmail.com>
2020-11-02 21:20:25 +0100

    Indentacion rota

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Centros.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Centros.kt
@@ -80,9 +80,9 @@ class Centros : Fragment() {
 
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_centros, container, false)
     }
