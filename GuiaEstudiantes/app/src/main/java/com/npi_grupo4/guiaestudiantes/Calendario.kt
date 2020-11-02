b19328df34ecac663202d6efb27883c1ca561e43
Alexmnzlms <alexmanza2@gmail.com>
2020-11-02 21:22:07 +0100

    Webview calendario academico

--- GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Calendario.kt
+++ GuiaEstudiantes/app/src/main/java/com/npi_grupo4/guiaestudiantes/Calendario.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,23 +23,61 @@ private const val ARG_PARAM2 = "param2"
  */
 class Calendario : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var webview: WebView
+    lateinit var pdf: String
+    lateinit var barra: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendario, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_comedores, container, false)
+        webview = view.findViewById(R.id.pdf_comedores) as WebView
+        barra = view.findViewById(R.id.progressBar2) as ProgressBar
+        webview.getSettings().setJavaScriptEnabled(true)
+        pdf = "https://docencia.ugr.es/sites/vicerrectorados_files/vic_docencia/public/inline-files/calendario_grado_master_2021%20%28definitivo%29.pdf"
+
+        webview.settings.javaScriptEnabled = true
+        webview.settings.setSupportZoom(true)
+        webview.settings.builtInZoomControls = true
+        webview.clearCache(true)
+
+        //Para que salga la barra al iniciar el fragment
+        webview.visibility = View.GONE
+        barra.visibility = View.VISIBLE
+
+        webview.webChromeClient = object : WebChromeClient() {
+
+            override fun onProgressChanged(view: WebView, progress: Int) {
+                if (progress < 100 && barra.visibility == ProgressBar.GONE) {
+                    barra.visibility = ProgressBar.VISIBLE
+                    webview.visibility = View.GONE
+                }
+                barra.progress = progress
+                if (progress == 100) {
+                    barra.visibility = ProgressBar.GONE
+                    webview.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                webview.loadUrl(request.url.toString())
+                return false
+            }
+        }
+
+        webview.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf)
+
+        return view
     }
 
     companion object {
@@ -44,16 +87,16 @@ class Calendario : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Calendario.
+         * @return A new instance of fragment Comedores.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Calendario().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                Comedores().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
